a18ac98d89a91feaaebf9703c06ace1a9be6b856
singer-songwriter <austinmesa+github@protonmail.com>
2026-01-14 19:52:23 -0500

    add population subplot

--- visualizer.py
+++ visualizer.py
@@ -25,10 +25,20 @@ class Visualizer:
         self.ax: plt.Axes | None = None
         self.img: Any = None
         self.anim: animation.FuncAnimation | None = None
+        self.population_history: list[int] = []
+        self.generation_history: list[int] = []
+        self.pop_line: Any = None
+        self.pop_ax: plt.Axes | None = None
 
     def _setup_figure(self) -> None:
         """Get the matplotlib window ready."""
-        self.fig, self.ax = plt.subplots(figsize=self.figsize)
+        self.fig, (self.ax, self.pop_ax) = plt.subplots(
+            1, 2,
+            figsize = (self.figsize[0] + 4, self.figsize[1]),
+            gridspec_kw = {"width_ratios" : [3, 1]},
+        )
+
+        # self.fig, self.ax = plt.subplots(figsize=self.figsize)
         self.ax.set_xticks(np.arange(-0.5, self.grid.width, 1), minor=True)
         self.ax.set_yticks(np.arange(-0.5, self.grid.height, 1), minor=True)
         self.ax.tick_params(which="both", length=0, labelbottom=False, labelleft=False)
@@ -40,7 +50,12 @@ class Visualizer:
             interpolation="nearest",
             vmin=0,
             vmax=1,
-        )
+        )   
+
+        self.pop_ax.set_xlabel("Generation")
+        self.pop_ax.set_ylabel("Population")
+        self.pop_ax.set_title("Population Over Time")
+        (self.pop_line,) = self.pop_ax.plot([], [], color="green", linewidth=1.5)
 
         self.stats_text = self.ax.text(
             0.02, 0.98,
@@ -53,6 +68,7 @@ class Visualizer:
         )
         
         self.stats_text.set_text(self._get_stats())
+        self._record_population()
         self._update_title()
 
     def _update_title(self) -> None:
@@ -65,14 +81,22 @@ class Visualizer:
         total = self.grid.width * self.grid.height
         percent = (alive / total) * 100
         return f"Alive: {alive} ({percent:.1f}%)"
+    
+    def _record_population(self) -> None:
+        self.generation_history.append(self.grid.generation)
+        self.population_history.append(int(np.sum(self.grid.cells)))
 
     def _animate_frame(self, frame: int) -> tuple[Any, ...]:
         """Called each frame - step the sim and redraw."""
         self.grid.step()
+        self._record_population()
         self.img.set_array(self.grid.cells)
         self._update_title()
+        self.pop_line.set_data(self.generation_history, self.population_history)
+        self.pop_ax.relim()
+        self.pop_ax.autoscale_view()
         self.stats_text.set_text(self._get_stats())
-        return (self.img, self.stats_text)
+        return (self.img, self.pop_line, self.stats_text)
 
     def animate(
         self,
